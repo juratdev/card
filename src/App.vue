6b399b2971430092f72b513760f3d417892193ef
juratdev <juratdev@gmail.com>
2024-05-01 18:26:46 +0500

    feat: add graph component

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,19 @@
 <script setup>
-import { ref } from "vue";
-import ApexCharts from "apexcharts";
 import Button from "./components/Button.vue";
-
-function generateDayWiseTimeSeries(s, count) {
-  let values = [
-    [4, 3, 10, 9, 29, 19, 25, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5],
-    [2, 3, 8, 7, 22, 16, 23, 7, 11, 5, 12, 5, 10, 4, 15, 2, 6, 2],
-  ];
-  let i = 0;
-  let series = [];
-  let x = new Date("11 Nov 2012").getTime();
-  while (i < count) {
-    series.push([x, values[s][i]]);
-    x += 86400000;
-    i++;
-  }
-  return series;
-}
-
-const series = ref([
-  {
-    name: "XYZ MOTORS",
-    data: [
-      {
-        name: "Series A",
-        data: generateDayWiseTimeSeries(1, 18),
-      },
-      {
-        name: "Series B",
-        data: generateDayWiseTimeSeries(1, 18),
-      },
-    ],
-  },
-]);
-const options = {
-  chart: {
-    type: "area",
-    stacked: false,
-    height: 350,
-    zoom: {
-      type: "x",
-      enabled: true,
-      autoScaleYaxis: true,
-    },
-    toolbar: {
-      autoSelected: "zoom",
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  markers: {
-    size: 0,
-  },
-  title: {
-    text: "Stock Price Movement",
-    align: "left",
-  },
-  fill: {
-    type: "gradient",
-    gradient: {
-      shadeIntensity: 1,
-      inverseColors: false,
-      opacityFrom: 0.5,
-      opacityTo: 0,
-      stops: [0, 90, 100],
-    },
-  },
-  yaxis: {
-    labels: {
-      formatter: function (val) {
-        return (val / 1000000).toFixed(0);
-      },
-    },
-    title: {
-      text: "Price",
-    },
-  },
-  xaxis: {
-    type: "datetime",
-  },
-  tooltip: {
-    shared: false,
-    y: {
-      formatter: function (val) {
-        return (val / 1000000).toFixed(0);
-      },
-    },
-  },
-};
+import Graph from "./components/Graph.vue";
 </script>
 
 <template>
   <div class="container px-4 mx-auto mt-10">
-    <div class="w-[790px] bg-black p-8 rounded-[20px]">
+    <div class="w-[700px] bg-black p-8 rounded-[20px]">
       <table class="text-left text-white">
         <thead>
           <tr class="text-sm font-medium whitespace-nowrap text-white-rgba">
-            <td class="pr-[52px]">Trading pair</td>
-            <td class="pr-[52px]">Ask price</td>
-            <td class="pr-[52px]">Bid price</td>
-            <td class="pr-[52px]">Spread</td>
-            <td class="pr-[52px]">Market</td>
+            <td class="pr-[48px]">Trading pair</td>
+            <td class="pr-[48px]">Ask price</td>
+            <td class="pr-[48px]">Bid price</td>
+            <td class="pr-[48px]">Spread</td>
+            <td class="pr-[48px]">Market</td>
             <td>Action</td>
           </tr>
         </thead>
@@ -131,13 +42,13 @@ const options = {
             <td class="text-primary">1.4451</td>
             <td class="text-primary">1.4458</td>
             <td>2</td>
-            <td>graph</td>
+            <td><Graph /></td>
             <td>
               <Button title="Trade" variant="default" />
             </td>
           </tr>
           <tr>
-            <td class="">
+            <td>
               <div class="flex flex-col">
                 <span class="font-medium">EUR/USD</span>
                 <div class="flex items-center gap-1">
@@ -160,11 +71,11 @@ const options = {
             <td class="text-secondary">1.4451</td>
             <td class="text-secondary">1.4458</td>
             <td class="">2</td>
-            <td>graph</td>
+            <td><Graph /></td>
             <td><Button title="Trade" variant="default" /></td>
           </tr>
           <tr>
-            <td class="">
+            <td>
               <div class="flex flex-col">
                 <span class="font-medium">EUR/USD</span>
                 <div class="flex items-center gap-1">
@@ -187,24 +98,46 @@ const options = {
             <td class="text-primary">1.4451</td>
             <td class="text-primary">1.4458</td>
             <td class="">2</td>
-            <td>graph</td>
+            <td><Graph /></td>
             <td>
               <Button title="Trade" variant="default" />
             </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="flex flex-col">
+                <span class="font-medium">EUR/USD</span>
+                <div class="flex items-center gap-1">
+                  <span class="text-xs text-secondary">+0.05%</span>
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                  >
+                    <path
+                        d="M5.0042 9.17999C4.57795 9.17999 4.18336 8.96291 3.95753 8.61374L3.91461 8.54208L0.956282 3.43791L0.930448 3.38541C0.856543 3.20263 0.82692 3.00497 0.843996 2.80856C0.861073 2.61215 0.924367 2.42256 1.02871 2.25528C1.13305 2.088 1.27548 1.94778 1.44436 1.84606C1.61324 1.74434 1.80379 1.68401 2.00045 1.66999L2.08336 1.66666L7.9167 1.66666L7.93711 1.66791L7.98378 1.66708C8.16936 1.67663 8.35048 1.72743 8.51395 1.8158C8.67743 1.90417 8.81914 2.02787 8.92878 2.17791L8.9742 2.24458C9.07343 2.40048 9.13686 2.57645 9.15991 2.75982C9.18297 2.94318 9.16508 3.12937 9.10753 3.30499L9.07753 3.38583L9.05253 3.43791L6.09753 8.53499C5.98945 8.7304 5.831 8.89328 5.63865 9.00671C5.44631 9.12014 5.22708 9.17998 5.00378 9.17999H5.0042Z"
+                        fill="#E03528"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </td>
+            <td class="text-secondary">1.4451</td>
+            <td class="text-secondary">1.4458</td>
+            <td class="">2</td>
+            <td><Graph /></td>
+            <td><Button title="Trade" variant="default" /></td>
           </tr>
         </tbody>
       </table>
     </div>
 
-    <div id="chart">
-      <apexchart
-        type="area"
-        height="350"
-        :options="options"
-        :series="series"
-      ></apexchart>
-    </div>
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/Graph.vue
@@ -0,0 +1,81 @@
+<script setup>
+import ApexCharts from "apexcharts";
+
+const series =  [{
+  name: "Sample Data",
+  data: [20,700,200, 400, 800, 50,79, 400]
+}]
+
+
+const chartOptions = {
+  chart: {
+    background: 'transparent',
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
+    toolbar: {
+      show: false,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  options: {
+    chart: {
+      background: 'none',
+    }
+  },
+  tooltip: {
+    enabled: false,
+  },
+  grid: {
+    show: false,
+  },
+  xaxis: {
+    show: false,
+    labels: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: false,
+    labels: {
+      show: false,
+    }
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 3,
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'none',
+      type: "horizontal",
+      shadeIntensity: 0.5,
+      gradientToColors: undefined,
+      inverseColors: false,
+      opacityFrom: 0,
+      opacityTo: 0,
+    }
+  }
+
+};
+</script>
+
+<template>
+  <div id="chart">
+    <apexchart type="area" width="109px" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
